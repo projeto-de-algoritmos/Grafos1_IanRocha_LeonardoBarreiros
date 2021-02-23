55811be6e090030289740ee1d1acac109ee47190
Ian Rocha <ianrocha.df@gmail.com>
2021-02-22 22:37:55 -0300

    Implementando lógica do player

--- src/Player.py
+++ src/Player.py
@@ -1,32 +1,49 @@
-class Player:
-    def __init__(self):
-        self.x = 40
-        self.y = 0
-        self.playerHeight = 35
-        self.playerWidth = 35
-        self.movementSpeed = 0.1
-        self.imagePlayer = None
+import pygame
 
-    def moveUp(self):
-        if (self.y < 0):
-            self.y = 0
+class Player(pygame.sprite.Sprite):
+    def __init__(self, image, pos, background):
+        super().__init__()
+        self.image = image
+        self.pos = pygame.Vector2(pos)
+        self.rect = self.image.get_rect(center=self.pos)
+        self.background = background
 
-        self.y = self.y - self.movementSpeed
+    def update(self, grid, events, dt, width, cols, rows):
+        keyEvent = pygame.key.get_pressed()
+        move = pygame.Vector2((0, 0))
 
-    def moveDown(self, limit):
-        if (self.y >= limit):
-            self.y = limit
+        # calculate maximum movement and current cell position
+        testdist = dt // 5 + 2
+        cellx = self.rect.centerx // width
+        celly = self.rect.centery // width
+        minx = self.rect.left // width
+        maxx = self.rect.right // width
+        miny = self.rect.top // width
+        maxy = self.rect.bottom // width
 
-        self.y = self.y + self.movementSpeed
+        # test move up
+        if minx == maxx and keyEvent[pygame.K_UP]:
+            nexty = (self.rect.top-testdist) // width
+            if celly == nexty or (nexty >= 0 and not grid[celly][cellx].walls[0]):
+                move += (0, -1)
 
-    def moveRight(self, limit):
-        if (self.x >= limit):
-            self.x = limit
+        # test move right
+        elif miny == maxy and keyEvent[pygame.K_RIGHT]:
+            nextx = (self.rect.right+testdist) // width
+            if cellx == nextx or (nextx < cols and not grid[celly][cellx].walls[1]):
+                move += (1, 0)
 
-        self.x = self.x + self.movementSpeed
+        # test move down
+        elif minx == maxx and keyEvent[pygame.K_DOWN]:
+            nexty = (self.rect.bottom+testdist) // width
+            if celly == nexty or (nexty < rows and not grid[celly][cellx].walls[2]):
+                move += (0, 1)
 
-    def moveLeft(self):
-        if (self.x < 0):
-            self.x = 0
+        # test move left
+        elif miny == maxy and keyEvent[pygame.K_LEFT]:
+            nextx = (self.rect.left-testdist) // width
+            if cellx == nextx or (nextx >= 0 and not grid[celly][cellx].walls[3]):
+                move += (-1, 0)
 
-        self.x = self.x - self.movementSpeed
+        self.pos = self.pos + move*(dt/5)
+        self.rect.center = self.pos
