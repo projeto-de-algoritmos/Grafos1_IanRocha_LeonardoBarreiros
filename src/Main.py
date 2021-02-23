6f5b883b3554ba35033880d6152717d7474ba80a
Ian Rocha <ianrocha.df@gmail.com>
2021-02-22 23:09:55 -0300

    Ajustando main e inserindo lógica para player e gerar labirinto
    inserindo lógica para gerar walls, renderizar player e background

--- src/Main.py
+++ src/Main.py
@@ -1,85 +1,132 @@
-from pygame.locals import *
-from Player import *
-from Display import *
-from Maze import *
+import random
 import pygame
+from Color import *
+from Player import *
+from Cell import *
 
+pygame.init()
 
-class Game:
+windowSize = (800, 700)
+screen = pygame.display.set_mode(windowSize)
+pygame.display.set_caption("Random Maze")
 
-    def __init__(self):
-        self.running = True
-        self.display_game = Display()
-        self.player = Player()
-        self.maze = Maze()
+done = False
 
-    def on_init(self):
-        pygame.init()
-        self.running = True
-        self.display_game = pygame.display.set_mode((self.display_game.windowWidth,self.display_game.windowHeight), pygame.HWSURFACE)
+clock = pygame.time.Clock()
 
-        pygame.display.set_caption('Random Maze')
-        playerImage = pygame.image.load("./assets/player.png").convert()
-        blockImage = pygame.image.load("./assets/block.png").convert()
-        self.player.imagePlayer = pygame.transform.scale(playerImage, (self.player.playerWidth, self.player.playerHeight))
-        self.maze.blockImage = pygame.transform.scale(blockImage, (35, 35))
+width = 25 # largura de cada cell
+cols = int(windowSize[0] / width)
+rows = int(windowSize[1] / width)
 
-    def isCollision(self, posX, posY, bsize):
-        if posX * 15 != bsize:
-            if posY * 15 != bsize:
-                return True
-        return False
+stack = []
 
-    def on_event(self, event):
-        if event.type == QUIT:
-            self.running = False
+pos = (0,0)
 
-    def on_loop(self):
-        pass
-    #     for block in range(0, self.maze.col * self.maze.row):
-    #         if self.isCollision(self.player.x, self.player.y, 35):
-    #             self.player.x, self.player.y = self.player.x, self.player.y
+def load_background():
+    background = pygame.Surface.fill(screen, Color.BLACK.value)
+    return background
 
-    def on_render(self):
-        self.display_game.fill((0,0,0))
-        self.display_game.blit(self.player.imagePlayer,(self.player.x,self.player.y))
-        self.maze.draw(self.display_game, self.maze.blockImage, 40, 40)
+def load_player(background):
+    player = pygame.Surface((10, 10))
+    player.fill(Color.BLUE.value)
+
+    posx = random.randint(0, rows-1) * width + width//2
+    posy = random.randint(0, cols-1) * width + width//2
+
+    return Player(player, (posx, posy), background)
+
+def removeWalls(current_cell,next_cell):
+    x = int(current_cell.x / width) - int(next_cell.x / width)
+    y = int(current_cell.y / width) - int(next_cell.y / width)
+    if x == -1: # right of current
+        current_cell.walls[1] = False
+        next_cell.walls[3] = False
+    elif x == 1: # left of current
+        current_cell.walls[3] = False
+        next_cell.walls[1] = False
+    elif y == -1: # bottom of current
+        current_cell.walls[2] = False
+        next_cell.walls[0] = False
+    elif y == 1: # top of current
+        current_cell.walls[0] = False
+        next_cell.walls[2] = False
+
+grid = []
+
+for y in range(rows):
+    grid.append([])
+    for x in range(cols):
+        grid[y].append(Cell(x, y, width))
+
+current_cell = grid[0][0]
+next_cell = 0
+
+def main():
+    global current_cell
+
+    player = None
+    initialized = False
+    current_maze = None
+    frame = 0
+    screen_rect = screen.get_rect()
+    clock = pygame.time.Clock()
+    sprites = pygame.sprite.Group()
+
+    if not initialized:
+        background = load_background()
+        background = None
+        player = load_player(background)
+        sprites.add(player)
+        initialized = True
+
+    play = False
+
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        for y in range(rows):
+            for x in range(cols):
+                grid[y][x].draw(screen, width)
+
+        if play == True:
+            pygame.init()
+
+            while not done:
+                events = pygame.event.get()
+
+                for e in events:
+                    if e.type == pygame.QUIT:
+                        return
+
+                player_x = player.pos[0]
+                player_y = player.pos[1]
+                pygame.draw.rect(screen, Color.LIGHTSKYBLUE.value, player.rect)
+
+                sprites.update(grid, None, frame, width, cols, rows)
+
+                sprites.draw(screen)
+                pygame.display.flip()
+                frame = clock.tick(60)
+        else:
+            current_cell.visited = True
+            current_cell.current = True
+
+            next_cell = current_cell.checkNeighbors(grid, width, rows, cols)
+            if next_cell != False:
+                current_cell.neighbors = []
+                stack.append(current_cell)
+                removeWalls(current_cell,next_cell)
+                current_cell.current = False
+                current_cell = next_cell
+            elif len(stack) > 0:
+                current_cell.current = False
+                current_cell = stack.pop()
+            else:
+                play = True
         pygame.display.flip()
 
-    def on_cleanup(self):
-        pygame.quit()
-
-    def on_execute(self):
-
-        limitHeight = self.display_game.windowHeight - self.player.playerHeight
-        limitWidth = self.display_game.windowWidth - self.player.playerWidth
-
-        if self.on_init() == False:
-            self.running = False
-
-        while(self.running):
-            pygame.event.pump()
-            keyEvent = pygame.key.get_pressed()
-
-            if (keyEvent[K_UP]):
-                self.player.moveUp()
-
-            if (keyEvent[K_DOWN]):
-                self.player.moveDown(limitHeight)
-            if (keyEvent[K_RIGHT]):
-                self.player.moveRight(limitWidth)
-
-            if (keyEvent[K_LEFT]):
-                self.player.moveLeft()
-
-            if (keyEvent[K_ESCAPE]):
-                self.running = False
-
-            self.on_loop()
-            self.on_render()
-        self.on_cleanup()
-
-if __name__ == "__main__" :
-    game = Game()
-    game.on_execute()
-
+if __name__ == "__main__":
+    main()
+    pygame.quit()
--- src/Player.py
+++ src/Player.py
@@ -8,12 +8,11 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(center=self.pos)
         self.background = background
 
-    def update(self, grid, events, dt, width, cols, rows):
+    def update(self, grid, events, frame, width, cols, rows):
         keyEvent = pygame.key.get_pressed()
         move = pygame.Vector2((0, 0))
 
-        # calculate maximum movement and current cell position
-        testdist = dt // 5 + 2
+        testdist = frame // 5 + 2
         cellx = self.rect.centerx // width
         celly = self.rect.centery // width
         minx = self.rect.left // width
@@ -21,29 +20,29 @@ class Player(pygame.sprite.Sprite):
         miny = self.rect.top // width
         maxy = self.rect.bottom // width
 
-        # test move up
+        # move up
         if minx == maxx and keyEvent[pygame.K_UP]:
             nexty = (self.rect.top-testdist) // width
             if celly == nexty or (nexty >= 0 and not grid[celly][cellx].walls[0]):
                 move += (0, -1)
 
-        # test move right
+        # move right
         elif miny == maxy and keyEvent[pygame.K_RIGHT]:
             nextx = (self.rect.right+testdist) // width
             if cellx == nextx or (nextx < cols and not grid[celly][cellx].walls[1]):
                 move += (1, 0)
 
-        # test move down
+        # move down
         elif minx == maxx and keyEvent[pygame.K_DOWN]:
             nexty = (self.rect.bottom+testdist) // width
             if celly == nexty or (nexty < rows and not grid[celly][cellx].walls[2]):
                 move += (0, 1)
 
-        # test move left
+        # move left
         elif miny == maxy and keyEvent[pygame.K_LEFT]:
             nextx = (self.rect.left-testdist) // width
             if cellx == nextx or (nextx >= 0 and not grid[celly][cellx].walls[3]):
                 move += (-1, 0)
 
-        self.pos = self.pos + move*(dt/5)
+        self.pos = self.pos + move*(frame/5)
         self.rect.center = self.pos
