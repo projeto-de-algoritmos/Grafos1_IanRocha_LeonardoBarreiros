1759c0f7f8582e7f4ccce5c9784f9ec013df70a5
Ian Rocha <ianrocha.df@gmail.com>
2021-02-22 22:35:18 -0300

    Adicionando Enum de cores

---
+++ src/Color.py
@@ -0,0 +1,11 @@
+import enum
+
+class Color(enum.Enum):
+    WHITE = (230,230,230)
+    GREY = (20,20,20)
+    BLACK = (0,0,0)
+    PURPLE = (100,0,100)
+    RED = (255,0,0)
+    GREEN = (0,255,0)
+    BLUE = (0,0,255)
+    LIGHTSKYBLUE = (135,206,250)
